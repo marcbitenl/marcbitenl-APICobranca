60e2df469db7cd96148a0d3fbec689d0a9cfa199
marcbitenl <marcbitenl@gmail.com>
2020-03-31 18:27:48 -0300

    Montando o combo dinamicamente

--- src/main/java/com/marcosbitencourt/cobranca/controller/TituloController.java
+++ src/main/java/com/marcosbitencourt/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.marcosbitencourt.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.marcosbitencourt.cobranca.model.StatusTitulo;
 import com.marcosbitencourt.cobranca.model.Titulo;
 import com.marcosbitencourt.cobranca.repository.Titulos;
 
@@ -20,8 +25,10 @@ public class TituloController {
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv ;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -31,6 +38,12 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Titulo salvo com sucesso");
 		return mv;
+	}
+	
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+		
 	}
 	
 }
